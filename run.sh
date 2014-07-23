3c12f492495466675b1e4492f678674553ea5396
Parker Abercrombie <parker@TheOkoriGroup.com>
2014-07-23 09:09:29 -0700

    GeoServer example class name changed, adjusted launch script.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Run a WorldWind Demo
 
-java -Xmx512m -Dsun.java2d.noddraw=true -classpath ./build:./lib/worldwind.jar:./lib/worldwindx.jar:./lib/jogl-all.jar:./lib/gluegen-rt.jar:./lib/gdal.jar com.pabercrombie.worldwindx.GeoserverElevations
+java -Xmx512m -Dsun.java2d.noddraw=true -classpath ./build:./lib/worldwind.jar:./lib/worldwindx.jar:./lib/jogl-all.jar:./lib/gluegen-rt.jar:./lib/gdal.jar com.pabercrombie.worldwindx.GeoServerElevations
